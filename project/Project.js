7cd0abc8310127d651c00038fcdaa639624b5d40
AlexTheFirstOne <alexa.ognjanovic@gmail.com>
2018-11-07 16:17:07 +0100

    Enabling Feature (Without spell corection)

--- project/Project.js
+++ project/Project.js
@@ -1,20 +1,19 @@
 $(window).ready(function(){
-    
-    // var vh = $(window).height()/100;
-    // var vw = $(window).width()/100;
-    // $(window).resize(function(){
-    //     var vh = $(window).height()/100;
-    //     var vw = $(window).width()/100;
-    // });
-    // $(window).scroll(function(){
-    //     var scroll=$(window).scrollTop();
-    //     if(scroll<30*vh){
-    //         var value = (50*vw-$(".preview h1").width()/2)/(40*vh-$(".preview h1").height())*scroll;
-    //         $(".preview h1").css({"left":50*vw-value+"px","position":"absolute","bottom":"0","transform":"translate(-50%,0%)"});
-    //     }else{
-    //         $(".preview h1").css({"position":"fixed","bottom":"100%","transform":"translate(-50%,100%)"});
-    //     }
-    // });
+    var vh = $(window).height()/100;
+    var vw = $(window).width()/100;
+    $(window).resize(function(){
+        var vh = $(window).height()/100;
+        var vw = $(window).width()/100;
+    });
+    $(window).scroll(function(){
+        var scroll=$(window).scrollTop();
+        if(scroll<30*vh){
+            var value = (50*vw-$(".preview h1").width()/2)/(40*vh-$(".preview h1").height())*scroll;
+            $(".preview h1").css({"left":50*vw-value+"px","position":"absolute","bottom":"0","transform":"translate(-50%,0%)"});
+        }else{
+            $(".preview h1").css({"position":"fixed","bottom":"100%","transform":"translate(-50%,100%)"});
+        }
+    });
 });
 
 
