3c695af6d3bd5e2f3378b110dde69167367865bf
Alexa Ognjanovic <alexa.ognjanovic@gmail.com>
2019-02-03 13:06:33 +0100

    Update test.py

--- packages/test.py
+++ packages/test.py
@@ -1,1 +1,7 @@
-print("Hello, World")
+import os
+def install():
+    os.rename(os.path.abspath(__file__), os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\packages\\"+os.path.basename(__file__))
+    print("done!")
+def use():
+    print("Use func")
+    
