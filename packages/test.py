b551475af6a312d713c90d1f7ac5e0e4d5bd3a15
Alexa Ognjanovic <alexa.ognjanovic@gmail.com>
2019-03-30 09:30:48 +0100

    Update test.py

--- packages/test.py
+++ packages/test.py
@@ -1,7 +1,1 @@
-import os
-def install():
-    os.rename(os.path.abspath(__file__), os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"\\packages\\"+os.path.basename(__file__))
-    print("done!")
-def use():
-    print("Use func")
-    
+print("it works!")
