3c517b53dfe16f043af1c223a86bce11a2adb799
GrbavaCigla <alexa.ognjanovic@gmail.com>
2019-12-10 16:24:13 +0100

    Finished the website finally

--- Home.js
+++ Home.js
@@ -1,17 +1,13 @@
-$(document).ready(function(){
-	var h=$(window).height();
-	var w=$(window).width();
-	if(h/w>512/217){
-		$(".bar").hide();
+$(document).ready(function() {
+	var links = ["https://api.github.com/users/GrbavaCigla/repos","https://api.github.com/orgs/PiSmartTV/repos"]
+	for (var i = links.length - 1; i >= 0; i--) {
+		$.getJSON(links[i],function(data){
+			data.forEach(function(obj){
+				console.log(obj);
+				if(obj.description!=null && obj.fork!=true){
+					$(".content").append('<div class="grid" data-aos="fade-right" data-aos-duration="800"><p class="title">'+ obj.name + '</p><p>'+ obj.description +'</p><a href="' + obj.html_url+ '">Visit</a></div>')
+				}
+			});
+		});
 	}
-	$(window).resize(function(){
-		var h=$(window).height();
-		var w=$(window).width();
-		if(h/w>250/114){
-			$(".bar").hide();
-		}else{
-			$(".bar").show();
-		}
-	});
-	
-}); 
+});
